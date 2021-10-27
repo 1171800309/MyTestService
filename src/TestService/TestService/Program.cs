6e5c1faa74f8f860cef3081e51c7924784a15b1e
bely <1171800309@qq.com>
2021-10-27 17:17:46 +0800

    提交文件

--- src/TestService/TestService/Program.cs
+++ src/TestService/TestService/Program.cs
@@ -17,7 +17,7 @@ namespace TestService
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>    
             Host.CreateDefaultBuilder(args)
             .UseServiceProviderFactory(new AutofacServiceProviderFactory()) //<--NOTE THIS
                 .ConfigureWebHostDefaults(webBuilder =>
